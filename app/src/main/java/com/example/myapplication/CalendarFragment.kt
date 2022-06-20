d77303828d1b21742cef379fe461edd2faa8ccf4
Parathan Vakeesan <parathan243@gmail.com>
2022-06-20 12:54:51 -0400

    Add responsiveness to Calendar and format layout

--- app/src/main/java/com/example/myapplication/CalendarFragment.kt
+++ app/src/main/java/com/example/myapplication/CalendarFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.CalendarView
 import android.widget.ListView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.myapplication.databinding.FragmentCalendarBinding
 
@@ -19,6 +21,8 @@ class CalendarFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    lateinit var dateTV: TextView
+    lateinit var calendarView: CalendarView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -42,7 +46,44 @@ class CalendarFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // initializing variables of
+        // list view with their ids.
+        calendarView = view.findViewById(R.id.calendar)
+        dateTV = view.findViewById(R.id.textview_date)
 
+        val Day1 = arrayOf("Medicine 1", "Reminder 2", "Medicine 2")
+        val Day2 = arrayOf("Medicine 1", "Medicine 3")
+
+        // on below line we are adding set on
+        // date change listener for calendar view.
+        calendarView
+            .setOnDateChangeListener(
+                CalendarView.OnDateChangeListener { _, year, month, dayOfMonth ->
+                    // In this Listener we are getting values
+                    // such as year, month and day of month
+                    // on below line we are creating a variable
+                    // in which we are adding all the cariables in it.
+                    val date = (dayOfMonth.toString() + "-"
+                            + (month + 1) + "-" + year)
+
+                    val l: ListView = view.findViewById(R.id.listCalendar)
+
+                    var data = arrayOf<String>()
+                    if (dayOfMonth % 2 == 0) {
+                        data = Day1
+
+                    } else {
+                        data = Day2
+                    }
+                    val listViewAdapter = ArrayAdapter(
+                        requireActivity(),
+                        android.R.layout.simple_list_item_1,
+                        data
+                    )
+                    l.adapter = listViewAdapter
+                    // set this date in TextView for Display
+                    dateTV.text = date
+                })
 
     }
 
