f9af1028c8b7ae811937fbac67650fbd57f123f0
enakohli <ekohli@uwaterloo.ca>
2022-06-21 10:26:38 -0400

    reformat code, update layout, comments

--- app/src/main/java/com/example/myapplication/CalendarFragment.kt
+++ app/src/main/java/com/example/myapplication/CalendarFragment.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -13,7 +14,6 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 //TODO: NotificationManager/AlarmManager + Hour+minutes + Sort by time
-//TODO: BUG: List is cut off by one element
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -27,105 +27,79 @@ class CalendarFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        //creating a list of items with custom adapter
         val view = inflater.inflate(R.layout.fragment_calendar, container, false)
         val listItems = arrayOf("Item 1", "Item 2")
         val l: ListView = view.findViewById(R.id.listCalendar)
-        //TODO: Custom adapter for subtext for medicine
-        val listViewAdapter = ArrayAdapter(
-            requireActivity(),
-            android.R.layout.simple_list_item_1,
-            listItems
-        )
-        //l.adapter = listViewAdapter
         l.adapter = CalendarListAdapter(requireActivity(), listItems)
         return view
     }
 
+    @SuppressLint("SimpleDateFormat")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         calendarView = view.findViewById(R.id.calendar)
         dateTV = view.findViewById(R.id.textview_date)
 
-        val Day1 = arrayOf("Norvasc", "Libitor", "Warfarin", "Brilinta", "Test 5", "Test 6", "Test 7", "Test 8", "Test 9")
-        val Day2 = arrayOf("Norvasc")
-        val Day3 = arrayOf("Norvasc", "Libitor")
-        val Day4 = arrayOf("Norvasc")
-        val Day5 = arrayOf("Norvasc", "Libitor", "Warfarin")
-        val Day6 = arrayOf("Norvasc", "Brilinta")
-        val Day7 = arrayOf("Norvasc", "Libitor")
+        //temp mockup data
+        val day1 = arrayOf("Norvasc", "Libitor", "Warfarin", "Brilinta", "Test 5", "Test 6", "Test 7", "Test 8", "Test 9")
+        val day2 = arrayOf("Norvasc")
+        val day3 = arrayOf("Norvasc", "Libitor")
+        val day4 = arrayOf("Norvasc")
+        val day5 = arrayOf("Norvasc", "Libitor", "Warfarin")
+        val day6 = arrayOf("Norvasc", "Brilinta")
+        val day7 = arrayOf("Norvasc", "Libitor")
 
         //Initialize date
-        //val sdf = SimpleDateFormat("dd-MM-yyyy")
         val sdf = SimpleDateFormat("EEE, MMM d, ''yy")
         val date = sdf.format(calendarView.date)
-        //val date = calendarView.date
-        val l: ListView = view.findViewById(R.id.listCalendar)
-        var data = arrayOf<String>()
-        /*
-        data = if (calendarView.date  % 2 == 0) {
-            Day1
-        } else {
-            Day2
-        }
-         */
 
-        data = Day1
-        val listViewAdapter = ArrayAdapter(
-            requireActivity(),
-            android.R.layout.simple_list_item_1,
-            data
-        )
-        //l.adapter = listViewAdapter
-        l.adapter = CalendarListAdapter(requireActivity(), data)
+        //list of medications
+        val l: ListView = view.findViewById(R.id.listCalendar)
+        l.adapter = CalendarListAdapter(requireActivity(), day1)
         // set this date in TextView for Display
         dateTV.text = date
 
-        //date change listener for calendar view.
+        //date change listener for calendar view
         calendarView
             .setOnDateChangeListener(
                 CalendarView.OnDateChangeListener { _, year, month, dayOfMonth ->
                     val calendar = Calendar.getInstance()
                     calendar.set(year, month, dayOfMonth)
-                    calendarView.date = calendar.getTimeInMillis()
+                    calendarView.date = calendar.timeInMillis
                     val sdf = SimpleDateFormat("EEE, MMM d, ''yy")
                     val date = sdf.format(calendarView.date)
-                    //val date = (dayOfMonth.toString() + "-" + (month + 1) + "-" + year)
+
+                    //show list of medicines
                     val l: ListView = view.findViewById(R.id.listCalendar)
                     var data = arrayOf<String>()
+
+                    //mockup data for one week
                     if (dayOfMonth % 7 == 0) {
-                        data = Day1
+                        data = day1
                     }
-                    else if (dayOfMonth % 7 == 1){
-                        data = Day2
+                    else if (dayOfMonth % 7 == 1) {
+                        data = day2
                     }
-                    else if (dayOfMonth % 7 == 2){
-                        data = Day3
+                    else if (dayOfMonth % 7 == 2) {
+                        data = day3
                     }
-                    else if (dayOfMonth % 7 == 3){
-                        data = Day4
+                    else if (dayOfMonth % 7 == 3) {
+                        data = day4
                     }
-                    else if (dayOfMonth % 7 == 4){
-                        data = Day5
+                    else if (dayOfMonth % 7 == 4) {
+                        data = day5
                     }
-                    else if (dayOfMonth % 7 == 5){
-                        data = Day6
+                    else if (dayOfMonth % 7 == 5) {
+                        data = day6
                     }
-                    else if (dayOfMonth % 7 == 6){
-                        data = Day7
+                    else if (dayOfMonth % 7 == 6) {
+                        data = day7
                     }
-                    val listViewAdapter = ArrayAdapter(
-                        requireActivity(),
-                        android.R.layout.simple_list_item_1,
-                        data
-                    )
-                    //l.adapter = listViewAdapter
+
                     l.adapter = CalendarListAdapter(requireActivity(), data)
                     // set this date in TextView for Display
                     dateTV.text = date
                 })
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
     }
 }
--- app/src/main/java/com/example/myapplication/CalendarListAdapter.kt
+++ app/src/main/java/com/example/myapplication/CalendarListAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import android.widget.TextView
 import java.util.*
 
 //Starter code from https://stackoverflow.com/questions/61321990/android-listview-add-text-to-the-end-of-row
+//create custom adapter
 class CalendarListAdapter(context: Context, data: Array<String>) : BaseAdapter() {
     private val context: Context
     private val data: Array<String>
@@ -24,19 +26,23 @@ class CalendarListAdapter(context: Context, data: Array<String>) : BaseAdapter()
         return i.toLong()
     }
 
+   @SuppressLint("InflateParams")
    override fun getView(i: Int, view: View?, viewGroup: ViewGroup?): View? {
+       //random times for medicine
        val times = arrayOf("9:00am", "5:30pm", "8:00pm", "6:00pm", "11:00am", "9:30pm", "3:15pm")
        var view: View? = view
-        if (view == null) {
-            view = LayoutInflater.from(context).inflate(R.layout.calendar_list_item, null)
-        }
-        val name: TextView = view!!.findViewById(R.id.name)
-        name.text = data[i]
-        val time: TextView = view!!.findViewById(R.id.time)
+       if (view == null) {
+           view = LayoutInflater.from(context).inflate(R.layout.calendar_list_item, null)
+       }
+       val name: TextView = view!!.findViewById(R.id.name)
+       name.text = data[i]
+
+       //set random time
+       val time: TextView = view.findViewById(R.id.time)
        val randomIndex: Int = Random().nextInt(times.size)
-       val randomName: String = times[randomIndex]
-       time.text = randomName
-        return view
+       val medTime: String = times[randomIndex]
+       time.text = medTime
+       return view
     }
 
     init {
