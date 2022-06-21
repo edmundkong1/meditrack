03d539721fe60a26c97369ae1930ac6f3ec69f6a
enakohli <ekohli@uwaterloo.ca>
2022-06-21 09:45:35 -0400

    fix listview scrolling layout, update medicine times randomly, fix formatting

--- app/src/main/java/com/example/myapplication/CalendarListAdapter.kt
+++ app/src/main/java/com/example/myapplication/CalendarListAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.TextView
+import java.util.*
 
 //Starter code from https://stackoverflow.com/questions/61321990/android-listview-add-text-to-the-end-of-row
 class CalendarListAdapter(context: Context, data: Array<String>) : BaseAdapter() {
@@ -24,14 +25,17 @@ class CalendarListAdapter(context: Context, data: Array<String>) : BaseAdapter()
     }
 
    override fun getView(i: Int, view: View?, viewGroup: ViewGroup?): View? {
-        var view: View? = view
+       val times = arrayOf("9:00am", "5:30pm", "8:00pm", "6:00pm", "11:00am", "9:30pm", "3:15pm")
+       var view: View? = view
         if (view == null) {
             view = LayoutInflater.from(context).inflate(R.layout.calendar_list_item, null)
         }
         val name: TextView = view!!.findViewById(R.id.name)
         name.text = data[i]
         val time: TextView = view!!.findViewById(R.id.time)
-        time.text = "9:00am"
+       val randomIndex: Int = Random().nextInt(times.size)
+       val randomName: String = times[randomIndex]
+       time.text = randomName
         return view
     }
 
