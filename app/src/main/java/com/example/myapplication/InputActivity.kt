a2c2c5644669fc5aa738e2fdface81de5c91364a
enakohli <ekohli@uwaterloo.ca>
2022-06-24 10:22:13 -0400

    added input activity layout file

--- app/src/main/java/com/example/myapplication/InputActivity.kt
+++ app/src/main/java/com/example/myapplication/InputActivity.kt
@@ -1,40 +1,9 @@
 package com.example.myapplication
 
-import android.app.ActionBar
 import android.app.Activity
-import android.content.Intent
-import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.example.myapplication.databinding.ActivityInputBinding
-import com.example.myapplication.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 
 //new Activity created because Input tab is a new screen
 //TODO: need to add either the action bar at the bottom of this new screen, or a back button to go to previous screen
-class InputActivity : AppCompatActivity() {
+class InputActivity : Activity() {
     //TODO: add all input content here, and layout info to activity_input.xml file
-    private lateinit var binding: ActivityInputBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = ActivityInputBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-
-        // TODO: Don't hardcode name of tabs
-
-
-        //TODO: INPUT tab - action should lead to input fragment
-    }
 }
