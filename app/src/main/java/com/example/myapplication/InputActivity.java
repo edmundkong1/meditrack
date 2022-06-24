b907858f7623b1c9858ec3e070790bc4e0c628cc
enakohli <ekohli@uwaterloo.ca>
2022-06-24 10:22:13 -0400

    added input activity layout file

--- app/src/main/java/com/example/myapplication/InputActivity.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication;
-
-import android.app.Activity;
-
-//new Activity created because Input tab is a new screen
-//TODO: need to add either the action bar at the bottom of this new screen, or a back button to go to previous screen
-public class InputActivity extends Activity {
-    //TODO: add all input content here
-}
