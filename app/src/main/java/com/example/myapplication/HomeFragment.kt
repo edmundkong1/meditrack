315636d366a4dffee4f7e70e1782ca3093e157b8
Parathan Vakeesan <parathan243@gmail.com>
2022-06-30 23:38:45 -0400

    Show Current Date on Home Page

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -1,44 +1,48 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
+import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.FragmentHomeBinding
+import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.util.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
+    // @SuppressLint("NewApi")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        return binding.root
-
+        //creating a list of items with custom adapter
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    @SuppressLint("NewApi", "SimpleDateFormat")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val todayDate: TextView = view.findViewById(R.id.textview_currentDate)
 
-        binding.buttonFirst.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+
+        val date = Calendar.getInstance().time
+        val sdf = SimpleDateFormat("EEE, MMM d, ''yy")
+        val formattedDate = sdf.format(date)
+        todayDate.text = "Today: ".plus(formattedDate)
+
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        _binding = null
     }
 }
