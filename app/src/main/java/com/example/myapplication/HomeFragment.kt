0addf75bee8c7c091fb372d492c682d44558914f
enakohli <ekohli@uwaterloo.ca>
2022-07-03 12:33:58 -0400

    add button functionality to home tab, to switch to calendar tab

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -6,12 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListView
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
-import com.example.myapplication.databinding.FragmentHomeBinding
+import kotlinx.android.synthetic.main.fragment_home.*
 import java.text.SimpleDateFormat
-import java.time.LocalDate
 import java.util.*
 
 /**
@@ -34,13 +32,20 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val todayDate: TextView = view.findViewById(R.id.textview_currentDate)
 
-
         val date = Calendar.getInstance().time
         val sdf = SimpleDateFormat("EEE, MMM d, ''yy")
         val formattedDate = sdf.format(date)
         todayDate.text = "Today: ".plus(formattedDate)
 
+
+        //go to calendar fragment when button is clicked
+        home_calendar_button.setOnClickListener {
+            findNavController().navigate(R.id.action_Home_to_Calendar)
+        }
+
+
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
