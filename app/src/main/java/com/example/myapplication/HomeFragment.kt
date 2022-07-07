8f6e256a4ab622eab59f3023aba82ae968e12487
Parathan Vakeesan <parathan243@gmail.com>
2022-07-07 02:00:56 -0400

    Add Schedule Part to home page

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ListView
 import android.widget.TextView
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -39,6 +40,26 @@ class HomeFragment : Fragment() {
         val sdf = SimpleDateFormat("EEE, MMM d, ''yy")
         val formattedDate = sdf.format(date)
         todayDate.text = "Today: ".plus(formattedDate)
+
+        val data = arrayOf(arrayOf("Norvasc","9:00am", "medication", "Dosage: 5mg"), arrayOf("Physician Appointment", "2:00pm", "appointment", "Dr. Bad"))
+        val l: ListView = view.findViewById(R.id.listview_schedule)
+        l.adapter = CalendarListAdapter(requireActivity(), data)
+
+        // Would cause errors if below implementation done on empty array
+        // Will need to see how the data is going to be structured to check this
+        // Will no reminders be and empty array or nothing at all.
+        if (l.adapter.count > 0) {
+            var totalHeight = 0
+            for (i in 0..l.adapter.count - 1) {
+                val listItem = l.adapter.getView(i, null, l)
+                listItem.measure(0,0)
+                totalHeight += listItem.measuredHeight
+            }
+            val params = l.layoutParams
+            params.height = totalHeight + (l.dividerHeight * l.adapter.count-1)
+            l.layoutParams = params
+            l.requestLayout()
+        }
 
         //go to calendar fragment when button is clicked
         home_calendar_button.setOnClickListener {
