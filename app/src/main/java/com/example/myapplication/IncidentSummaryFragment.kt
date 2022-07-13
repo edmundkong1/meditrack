829ac8c444f938292e2620082fff4b420ad28794
Edmund <emkong@uwaterloo.ca>
2022-07-13 11:02:07 -0400

    Fixed null-pointer bug for IncidentSummaryFragment.kt

--- app/src/main/java/com/example/myapplication/IncidentSummaryFragment.kt
+++ app/src/main/java/com/example/myapplication/IncidentSummaryFragment.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -24,12 +25,15 @@ class IncidentSummaryFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        //lineChart = view?.findViewById(R.id.linechart)
-
-        //configureLineChart()
         // Inflate the layout for this fragment
-
         return inflater.inflate(R.layout.fragment_incident_summary, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        lineChart = view.findViewById(R.id.linechart)
+
+        configureLineChart()
     }
 
     private fun configureLineChart() {
