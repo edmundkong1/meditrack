3113d8dc0a69e1713b1c7bd7795370a67f0b2be9
Aditi Lohtia <alohtia@uwaterloo.ca>
2022-07-19 19:30:04 -0400

    merge

--- app/src/main/java/com/example/myapplication/Medications.kt
+++ app/src/main/java/com/example/myapplication/Medications.kt
@@ -2,7 +2,13 @@ package com.example.myapplication
 
 data class Medications(
     val name: String,
+    val timeHour: Int,
+    val timeMin: Int,
     val dosage: String,
     val actions: String,
     val directions: String,
+    val totalAmount: Int,
     var expandable: Boolean = true)
+
+
+
